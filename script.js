fac8b132ca25a5ae20181f2a8cea236d1179436c
Zach Gaines <zrgaines@mtu.edu>
2016-08-15 13:48:55 -0500

    jquery and animation working(sortof)

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 $( document ).ready(function() {
     $('#headerText').addClass('animated fadeInUp');
+
+    $('.projDiv').hover(
+      function(){
+        
+        $(this).parent().find('h2').removeClass('animated fadeOutLeft');
+        $(this).parent().find('h3').removeClass('animated fadeOutLeft');
+
+        $(this).parent().find('h2').addClass('animated fadeInLeft');
+        $(this).parent().find('h3').addClass('animated fadeInLeft');
+      }, function() {
+        $(this).parent().find('h2').removeClass('animated fadeInLeft');
+        $(this).parent().find('h3').removeClass('animated fadeInLeft');
+
+        $(this).parent().find('h2').addClass('animated fadeOutLeft');
+        $(this).parent().find('h3').addClass('animated fadeOutLeft');
+      }
+    )
 });
