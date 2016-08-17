dc3daa5401769162dd58501f67a864bffda8d68f
Zach Gaines <zrgaines@mtu.edu>
2016-08-16 21:50:24 -0500

    refactor

---
+++ blueballs.js
@@ -0,0 +1,80 @@
+// Initialization      
+      var container;
+      var camera, scene, renderer;
+      init();
+      animate();
+
+      //Blue balls
+      function init() {
+        container = document.getElementById( 'proj1' );
+
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / 300, 1, 5000 );
+        camera.position.y = -1000;
+
+        scene = new THREE.Scene();
+        
+        // Lights, gotta be able to see shit
+        var light;
+        scene.add( new THREE.AmbientLight( 0x3385ff ) );
+        light = new THREE.DirectionalLight( 0x0066ff, 3);
+        light.position.set( 0,-2,0 );
+        scene.add( light );
+
+        // Material to be used in our sphere
+        // http://threejs.org/docs/index.html#Reference/Materials/MeshLambertMaterial
+        // The docs have all the properties you can mess with on the material, try adding some new ones, color, opacity, etc.
+        var material = new THREE.MeshLambertMaterial({
+          side: THREE.DoubleSide,
+          wireframe: true,
+          color: 0x6666ff
+        });
+        
+        // Define the geometry and pass material into mesh, then add to scene
+        // http://threejs.org/docs/index.html#Reference/Extras.Geometries/SphereGeometry
+        for(var i = 0; i < 50; i++) {
+          var x = Math.random() * 2000 - 1000;
+          var y = Math.random() * 2000 - 1000;
+          var z = Math.random() * 2000 - 1000;
+            var object = new THREE.Mesh( new THREE.SphereGeometry(100, 30, 10), material );
+        object.position.set( x, y, z );
+        scene.add( object );
+        }
+
+      
+
+        // More setup stuff for renderer
+        renderer = new THREE.WebGLRenderer({alpha:true});
+        renderer.setSize( window.innerWidth, 300 );
+        container.appendChild( renderer.domElement );
+        container.addEventListener( 'resize', onWindowResize, false );
+      }
+
+      // Makes it so it doesnt break when you adjust window size
+      function onWindowResize() {
+        camera.aspect = window.innerWidth / 300;
+        camera.updateProjectionMatrix();
+        renderer.setSize( window.innerWidth, 300 );
+      }
+
+      function animate() {
+        requestAnimationFrame( animate );
+        render();
+      }
+            
+      // Runs constantly, think of like an update loop
+      function render() {
+        var timer = Date.now() * 0.00001;
+        camera.position.x = Math.cos( timer * 50 ) * 200;
+        camera.position.y = Math.tan( timer * 50 ) * 200;
+        camera.position.z = Math.sin( timer * 50 ) * 200;
+        camera.lookAt( scene.position );
+        for ( var i = 0, l = scene.children.length; i < l; i ++ ) {
+          var object = scene.children[ i ];
+          
+          // Makes our sphere turn, try tweaking these
+          object.rotation.x = timer * 20;
+          object.rotation.y = timer * 15;
+          object.rotation.z = timer * 20;
+        }
+        renderer.render( scene, camera );
+      }
---
+++ round1.js
@@ -0,0 +1,59 @@
+      //learn 3
+      init2();
+      function init2() {
+        
+        container2 = document.getElementById( 'proj2' );
+
+        // heeere we go !
+        
+        var blue = new THREE.Color(0x7658ed);
+        var pink = new THREE.Color(0xfca4c5);
+
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / 300, 0.1, 1000 );
+
+        var renderer = new THREE.WebGLRenderer({ alpha: true });
+        renderer.setSize( window.innerWidth, 300 );
+        container2.appendChild( renderer.domElement );
+
+        //THREEx.WindowResize(renderer, camera);
+        var shape = [];
+        var shapeSpeed = .2;
+
+        geometry = new THREE.IcosahedronGeometry(2.5,0);
+        material = new THREE.MeshNormalMaterial({ color: 0x0000ff });
+        shape[0] = new THREE.Mesh( geometry, material );
+        shape[1] = new THREE.Mesh( geometry, material );
+        shape[2] = new THREE.Mesh( geometry, material );
+        shape[0].position.set(0,5,0);
+        shape[1].position.set(0,5,0);
+        shape[2].position.set(0,5,0);
+        scene.add(shape[0],shape[1],shape[2]);
+
+        var light = new THREE.PointLight(0xfca4c5);
+        light.position.set(0,250,0);
+        scene.add(light);
+
+        camera.position.set(3,4,10); // x y z
+
+        function render() {
+          requestAnimationFrame( render );
+
+          for (var i = 0; i < shape.length; i++) {
+            if(shape[i].position.x > 40) {
+              shape[i].position.x = -25;
+            }
+            shape[i].position.x += shapeSpeed;
+          }
+          shape[0].rotation.x += 0.035;
+          shape[0].rotation.y -= 0.005;
+          shape[1].rotation.y += 0.015;
+          shape[1].rotation.z -= 0.005;
+          shape[2].rotation.z -= 0.025;
+          shape[2].rotation.x += 0.005;
+
+          renderer.render(scene, camera);
+        }
+        render();
+        
+      }
